31c0fffa51e35c94eb13cfd14ea88a8e94d70388
bogdan_g0306 <bogdan.gusuleac@cybertech.ro>
2021-10-25 18:15:42 +0300

    search for movieAPI

--- source/movieAPI/urls.py
+++ source/movieAPI/urls.py
@@ -16,5 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework.authtoken import views
+from .views import searchForMovieView, findMovieView
 
-urlpatterns = []
+urlpatterns = [
+    url('search/', searchForMovieView, name='search'),
+    url('find/', findMovieView, name='find')
+]
