ea7778c84a6271ee7824b9c4b74ce9abf27e1ea0
bogdan_g0306 <bogdan.gusuleac@cybertech.ro>
2021-10-26 18:06:34 +0300

    multi profile users and users creation with fake data

--- source/authentication/urls.py
+++ source/authentication/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from rest_framework.authtoken import views
-from .views import register, dummyView, login
+from .views import register, dummyView, login, createNewProfile, getAllProfiles, updateProfile, deleteProfile, getFakeList
 
 urlpatterns = [
     url('api-token-auth/', views.obtain_auth_token, name='auth'),
     url('register/', register, name='register'),
     url('dummy/', dummyView, name='dummy'),
-    url('login/', login, name='login')
+    url('login/', login, name='login'),
+    url('create/profile/', createNewProfile, name='create-profile'),
+    url('get/profiles/', getAllProfiles, name='get-profiles'),
+    url('update/profile/', updateProfile, name='update-profile'),
+    url('delete/profile/', deleteProfile, name='delete-profile'),
+    url('get/fakes/', getFakeList, name='get-fake-persons')
 ]
--- source/authentication/views.py
+++ source/authentication/views.py
@@ -7,6 +7,7 @@ import hashlib
 import hmac
 import json
 import os
+from uuid import uuid4
 
 import requests
 from django.contrib.auth import authenticate
@@ -33,7 +34,9 @@ from rest_framework import status
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
 from django.contrib.auth.models import User
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ProfileSerializer
+from .models import Profile
+from .userManager import getPersonList, filterPersons
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -49,7 +52,7 @@ def register(request):
 
         return response
     else:
-        return JsonResponse(serializedUser.errors)
+        return JsonResponse({'error': serializedUser.errors})
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -65,3 +68,99 @@ def login(request):
 @permission_classes([IsAuthenticated])
 def dummyView(request):
     return JsonResponse({'good': 'u see this'})
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createNewProfile(request):
+    # return JsonResponse({'value': profileSerializer.is_valid()})
+    randomString = str(uuid4())[0:19]
+    profileName = request.data.get('profileName') if request.data.get('profileName') else randomString
+
+    found = Profile.objects.filter(profileName=profileName).first()
+    if found is not None:
+        # profileName = randomString
+        return JsonResponse({'error': 'a profile with this name already exists'})
+
+    profileSerializer = ProfileSerializer(data={'profileName': profileName})
+    if profileSerializer.is_valid():
+        newProfileDict = {
+            'profileName': profileSerializer.validated_data['profileName'],
+            'user': request.user,
+        }
+        try:
+            profile = profileSerializer.create(validated_data=newProfileDict)
+            return JsonResponse({'profile': ProfileSerializer.getJsonVariant(profile), 'error': None})
+        except Exception as e:
+            return JsonResponse({'error': e.args})
+    else:
+        return JsonResponse({'error': profileSerializer.errors})
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getAllProfiles(request):
+    newProfileList = []
+    profiles = Profile.objects.filter(user=request.user)
+    for profile in profiles:
+        newProfileList.append(ProfileSerializer.getJsonVariant(profile))
+    return JsonResponse({'results': newProfileList, 'number of profiles': len(profiles), 'error': None})
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def updateProfile(request):
+    newProfileName = request.GET.get('newProfileName')
+    profileName = request.GET.get('profileName')
+    if profileName is None or profileName == '':
+        return JsonResponse({'error': 'A profile name is needed to make the changed'})
+
+    if newProfileName is None or newProfileName == '':
+        return JsonResponse({'error': 'A new profile name is needed to make the change'})
+
+    profile = Profile.objects.filter(profileName=profileName).first()
+    if profile is None:
+        return JsonResponse({'error': 'A profile with this name was not found'})
+
+    serializedProfile = ProfileSerializer(data={'profileName': newProfileName})
+    if serializedProfile.is_valid():
+        try:
+            updatedProfile = serializedProfile.update(profile, serializedProfile.validated_data)
+            return JsonResponse({'result': ProfileSerializer.getJsonVariant(updatedProfile), 'error': None})
+        except Exception as e:
+            return JsonResponse({'error': e.args})
+    else:
+        return JsonResponse({'error': serializedProfile.errors})
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def deleteProfile(request):
+    profileName = request.GET.get('profileName')
+
+    if profileName is None or profileName == '':
+        return JsonResponse({'error': 'A profile name is needed to make the delete'})
+
+    profile = Profile.objects.filter(profileName=profileName).first()
+    if profile is None:
+        return JsonResponse({'error': 'A profile with this name was not found'})
+
+    profileJson = ProfileSerializer.getJsonVariant(profile)
+    profile.delete()
+
+    return JsonResponse({'result': profileJson, 'error': None})
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getFakeList(request):
+    listSize = request.GET.get('listSize')
+    gender = request.GET.get('gender')
+
+    # we can populate the database with some users
+    try:
+        if listSize is None:
+            response, seed = getPersonList(gender=gender)
+        else:
+            response, seed = getPersonList(int(listSize), gender=gender)
+
+        response = filterPersons(response)
+        return JsonResponse({'results': response, 'seed': seed, 'error': None})
+    except Exception as e:
+        return JsonResponse({'error': e})
