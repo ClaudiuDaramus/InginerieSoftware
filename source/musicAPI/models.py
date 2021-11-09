0df76f3a7545bb9faca9cfaf4d585305ee65adb9
speedypleath <gheorgheandrei13@gmail.com>
2021-11-09 09:43:56 +0200

    models for artist, album, song

--- source/musicAPI/models.py
+++ source/musicAPI/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=50)
+    spotify_url = models.URLField(max_length=200)
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    spotify_url = models.URLField(max_length=200)
+    
+class Song(models.Model):
+    name = models.CharField(max_length=100)
+    spotify_url = models.URLField(max_length=200)
+    artists = models.ManyToManyField(Artist)
+    genres = models.IntegerField()
