be28d2f142efd4e20cff39fd51089c0c536cffbe
CibotaruMatei <cibotaru.matei@yahoo.ro>
2021-11-17 13:40:48 +0200

    Implemented Spotify backend

--- source/musicAPI/models.py
+++ source/musicAPI/models.py
@@ -14,3 +14,11 @@ class Song(models.Model):
     spotify_url = models.URLField(max_length=200)
     artists = models.ManyToManyField(Artist)
     genres = models.IntegerField()
+
+class SpotifyToken(models.Model):
+    user = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    refresh_token = models.CharField(max_length=150)
+    access_token = models.CharField(max_length=150)
+    expires_in = models.DateTimeField()
+    token_type = models.CharField(max_length=50)
